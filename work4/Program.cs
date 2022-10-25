abb46c30f565645200378fa77cb34beb538b184e
ilia <115416335+Iliathebest@users.noreply.github.com>
2022-10-25 11:51:22 +0300

    work4

---
+++ work4/Program.cs
@@ -0,0 +1,29 @@
+﻿//что то принимают и что то возвращают
+
+// // string Method4(int count, string text)
+// {
+//     int i = 0;
+//     string result = String.Empty;
+//     while(i < count)
+//     {
+//         result = result+text;
+//          i++;
+//          return result;
+//     }
+// }
+// string rec = Method4(12,"acab");
+// Console.WriteLine(rec);
+
+string Metod4(int count, string text)
+{
+int i = 0;
+string result = String.Empty;
+while (i<count)
+{
+result = result + text;
+i++;
+}
+return result;
+}
+string res = Metod4(12, "acab");
+Console.WriteLine(res);
